c5d2c92660e269f7b75d9fda3520b054127a6053
Liv Nelson <livnelson@me.com>
2022-11-29 19:53:20 -0800

    commit tree ba3ffcdd8b12aa2be5e2f5623a63f89c7ff79891

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,58 @@
+require "pry"
+
 class Movie < ActiveRecord::Base
+    def self.find_all_movies_by_year(year)
+        Movie.where(year: year)
+    end
+
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(attribute)
+        Movie.find_by(attribute)
+    end
+
+    def self.find_movies_after_2002
+        Movie.where("release_date > ?", 2002)
+    end
+
+    # def update_with_attributes(title)
+    #     # self.update(title: title) - title is a hash... see below to pull out the title value
+    #     self.update(title: title[:title])
+          # binding.pry
+    # end
+
+    def update_with_attributes(attribute)
+        self.update(attribute)
+    end
+
+    def self.update_all_titles(title)
+        Movie.update(title: title)
+    end
+
+    def self.delete_by_id(id)
+        Movie.delete(id)
+    end
+
+    def self.delete_all_movies
+        Movie.delete_all
+    end
 
 end
